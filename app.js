94b23f87550d1549360fb8f45634ad0bb7e8ffb9
yanweiqing <yanweiqing@meicai.cn>
2017-11-06 15:54:04 +0800

    上传文件功能完成

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = new Koa()
 const views = require('koa-views')
 const json = require('koa-json')
 const onerror = require('koa-onerror')
-const bodyparser = require('koa-bodyparser')
+const bodyparser = require('koa-body')
 const logger = require('koa-logger')
 
 const index = require('./routes/index')
@@ -15,7 +15,7 @@ onerror(app)
 
 // middlewares
 app.use(bodyparser({
-  enableTypes:['json', 'form', 'text']
+  multipart: true
 }))
 app.use(json())
 app.use(logger())
--- routes/cos.js
+++ routes/cos.js
@@ -9,44 +9,78 @@ router.get('/', function (ctx, next) {
   ctx.body = 'this is a cos response!'
 })
 
-router.get('/bar', function (ctx, next) {
-  ctx.body = {
-    data: {
-      name: 'bar'
-    },
-    status: 1
-  }
-})
-
 /*
  *  demo: https://github.com/tencentyun/cos-nodejs-sdk-v5/blob/master/demo/demo.js
  */
-var params = {
+const COS_ID = {
   AppId: '1253837476',
   //Bucket: 'meowlife',
   SecretId: 'AKIDZ4PYadDL0Vr3BJdTPAlBPdw881sZkPah',
   SecretKey: 'HOJ2Z4RioiQ3Xpx772tZ5Wr35ypUzJUw',
 };
-var cos = new COS(params);
+const COS_CONFIG = {
+  Bucket: 'meowlife',
+  Region: 'ap-beijing',
+  RegionSimple: 'bj'
+};
+let cos = new COS(COS_ID);
+let unique_id = 0;
+const getKey = () => {
+  return '_cos_img_' + unique_id + (new Date()).getTime();
+}
 
+// 获取权限接口
 router.post('/auth/:key', function (ctx, next) {
   var auth = cos.getAuth({
-    Method: 'get',
-    Key: ctx.params.key
   });
-
   ctx.body = {
     data: {
       auth
-    }
+    }, ret: 1
   };
 });
+
+const putObject = function(fileObj, param) {
+  return new Promise(function (resolve, reject) {
+    let file = fs.readFileSync(fileObj.path);
+    let filename = param.setPath + '/' + getKey();
+    cos.putObject({
+      Bucket: COS_CONFIG.Bucket,
+      Region: COS_CONFIG.Region,
+      Key: filename,  // 必须
+      TaskReady: function (tid) {
+        TaskId = tid;
+      },
+      onProgress: function (progressData) {
+        console.log(JSON.stringify(progressData));
+      },
+      Body: file,
+      ContentLength: fileObj.size
+    }, function (err, data) {
+      if (err) reject(err);
+      if (data) resolve({
+        data,
+        imgUrl: 'https://' + COS_CONFIG.Bucket + '-' + COS_ID.AppId + '.cos' + COS_CONFIG.RegionSimple + '.myqcloud.com/' + filename
+      });
+    });
+  });
+}
+
+router.post('/upload', async (ctx, next) => {
+  let bodyData = ctx.request.body;
+  let res = await putObject(bodyData.files.file, bodyData.fields);
+
+  ctx.body = {
+    data: res,
+    ret: 1
+  }
+})
 
 router.get('/auth/check', function (ctx, next) {
   var filepath2 = path.resolve(__dirname, '1mb.out2.zip');
   cos.getObject({
-    Bucket: 'meowlife',
-    Region: 'ap-beijing',
+    Bucket: COS_CONFIG.Bucket,
+    Region: COS_CONFIG.Region,
     Key: 'user-images/test123.png',
     // Output: fs.createWriteStream(filepath2),
     onProgress: function (progressData) {
